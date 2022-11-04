4a92e772b4e07bf127423448d4140748b5782198
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2022-11-03 19:40:27 -0600

    Fix comment formatting, add MIT schpeel

--- src/sluj/core.clj
+++ src/sluj/core.clj
@@ -1,4 +1,9 @@
-;; Copyright Rawley Fowler & Contributors.
+;; Copyright Rawley Fowler & Contributors, under the MIT license.
+;; THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+;; NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+;; OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ;; https://github.com/rawleyfowler/sluj
 (ns sluj.core
   (:require [sluj.charmap :as c]
@@ -12,7 +17,6 @@
 
 (defn sluj
   "'Slugifies' a given string s, to make it usable in URI's. Specifically UTF-16 strings"
-  ;; Credits to u/MartinPuda on Reddit for reminding me I can destructure
   [s & {:keys [locale separator casing]
         :or {locale "none" separator "-" casing "lower"}
         :as opts}]
@@ -24,9 +28,9 @@
                        (w/stringify-keys (apply dissoc (get opts :charmap opts) spec-opts))
                        ((keyword locale) c/locales))]
     (-> s
-        (string/replace #"[:/?#\[\]@!$&'()*+,;=]" "") ;; Remove any reserved RFC 3986 characters
-        (replace-characters charmap) ;; Replace the characters with the charmap characters
+        (string/replace #"[:/?#\[\]@!$&'()*+,;=]" "") ; Remove any reserved RFC 3986 characters
+        (replace-characters charmap) ; Replace the characters with the charmap characters
         (->>
-         (re-seq #"\w+") ;; Separate the words into a list
-         (string/join separator)) ;; Join the words back with the separator
-        casing-fn))) ;; Apply the specified casing
+         (re-seq #"\w+") ; Separate the words into a list
+         (string/join separator)) ; Join the words back with the separator
+        casing-fn))) ; Apply the specified casing
